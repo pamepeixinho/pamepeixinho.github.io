4dd6f88fd79ff6756634a3eff1a51faad8ec1c2d
pamepeixinho <pameiupip@gmail.com>
2018-01-03 01:53:31 -0200

    remove css / use styled component instead

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import './globalStyle.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
