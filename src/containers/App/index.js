eca9dba6b257c1ff686d30fd49098564eda6cfd0
pamepeixinho <pameiupip@gmail.com>
2018-01-03 01:41:47 -0200

    first take in online resume structure

---
+++ src/containers/App/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import logo from '../../logo.svg';
+import './App.css';
+
+const Wrapper = styled.div`
+  background-color: red;
+  text-align: center;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
