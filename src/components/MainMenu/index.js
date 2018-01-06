057577abbcc213cf9ce9344f821114206348ae21
pamepeixinho <pameiupip@gmail.com>
2018-01-06 12:45:51 -0200

    use anchors and have main divs

---
+++ src/components/MainMenu/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import Scrollchor from 'react-scrollchor';
+
+import AnchorsName from '../../constants';
+
+const Anchor = styled(Scrollchor)`
+  margin: 5px;
+`;
+
+class MainMenu extends React.PureComponent {
+
+  goTo = (anchorName) => `#${anchorName}`
+
+  experience = this.goTo(AnchorsName.experience)
+  education = this.goTo(AnchorsName.education)
+  projects = this.goTo(AnchorsName.projects)
+  events = this.goTo(AnchorsName.events)
+  articles = this.goTo(AnchorsName.articles)
+  awards = this.goTo(AnchorsName.awards)
+  skills = this.goTo(AnchorsName.skills)
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <Anchor to="">About</Anchor>
+          <Anchor to={this.experience}>Experience</Anchor>
+          <Anchor to={this.education}>Education</Anchor>
+          <Anchor to={this.projects}>Projects</Anchor>
+          <Anchor to={this.articles}>Articles</Anchor>
+          <Anchor to={this.awards}>Awards</Anchor>
+          <Anchor to={this.events}>Events</Anchor>
+          <Anchor to={this.skills}>Skills</Anchor>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MainMenu;
--- src/containers/Content/index.js
+++ src/containers/Content/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import dimens from '../../assets/dimens';
-import Header from '../../components/Header';
+import MainMenu from '../../components/MainMenu';
+import AnchorsName from '../../constants';
+
+const Div = styled.div`
+  background-color: red;
+  margin-bottom: 16px;
+  height: 200px
+`;
 
 class Content extends React.PureComponent {
   render() {
     return (
       <React.Fragment>
-        <p>teste</p>
+        <MainMenu />
+
+        <Div id={AnchorsName.experience}>
+          experience
+        </Div>
+
+        <Div id={AnchorsName.education}>
+          education
+        </Div>
+
+        <Div id={AnchorsName.projects}>
+          projects
+        </Div>
+
+        <Div id={AnchorsName.events}>
+          events
+        </Div>
+
+        <Div id={AnchorsName.articles}>
+          articles
+        </Div>
+
+        <Div id={AnchorsName.awards}>
+          awards
+        </Div>
+
+        <Div id={AnchorsName.skills}>
+          skills
+        </Div>
+
+        {/* <footer id="footer"></footer> */}
       </React.Fragment>
     );
   }
