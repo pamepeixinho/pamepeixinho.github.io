5d46b7d6c12be7c853c5c08b6dbf3840cc27fcbf
pamepeixinho <pameiupip@gmail.com>
2018-01-25 21:53:37 -0200

    articles box first take

---
+++ src/containers/Content/Articles/messages.js
@@ -0,0 +1,21 @@
+/*
+ * Articles Messages
+ *
+ * This contains all the text for the Articles Container.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  wvcTitle: {
+    id: 'app.container.Content.Articles.wvcTitle',
+    defaultMessage: 'WVC',
+  },
+  wvcSubtitle: {
+    id: 'app.container.Content.Articles.wvcSubtitle',
+    defaultMessage: 'wvcSubtitle WVC',
+  },
+  wvcDescription: {
+    id: 'app.container.Content.Articles.wvcDescription',
+    defaultMessage: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor ',
+  },
+});
