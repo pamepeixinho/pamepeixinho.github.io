0db3c9eae5009ba5ec2eaa5d8242fbdb6a15eeff
Pamela Peixinho <pamela.peixinho@quintoandar.com.br>
2018-01-26 15:16:56 -0200

    update links (#27)
    update facebook and linkedin

--- src/constants.js
+++ src/constants.js
@@ -10,8 +10,8 @@ export const AnchorsName = {
 };
 
 export const SocialLinks = {
-  facebook: 'https://www.facebook.com/pamela.peixinho',
-  linkedin: 'https://www.linkedin.com/in/pamelapeixinho/',
+  facebook: 'https://www.facebook.com/pamepeixinho',
+  linkedin: 'https://www.linkedin.com/in/pamepeixinho/',
   github: 'https://github.com/pamepeixinho',
   twitter: 'https://twitter.com/pamepeixinho',
   instagram: 'https://www.instagram.com/pamepeixinho/',
