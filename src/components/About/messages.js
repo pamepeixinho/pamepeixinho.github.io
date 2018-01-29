192ec6feeb4df8e7f28adb536e218627016ecaab
pamepeixinho <pameiupip@gmail.com>
2018-01-29 17:40:10 -0200

    add mail

--- src/components/About/messages.js
+++ src/components/About/messages.js
@@ -8,6 +8,6 @@ export default defineMessages({
   miniDescription: {
     id: 'app.about.miniDescription',
     defaultMessage: `I am Software Engineer at startup QuintoAndar, developing Web application (PWA);
-    apps for iOS, Android; RESTful APIs. I have finished BS in Computer Science at FEI`,
+    apps for iOS, Android and RESTful APIs. I have finished BS in Computer Science at FEI (end of 2017)`,
   },
 });
--- src/components/SocialMediaRow/index.js
+++ src/components/SocialMediaRow/index.js
@@ -6,17 +6,29 @@ import SocialLink from '../SocialLink';
 import { SocialLinks } from '../../constants';
 import { Facebook, Instagram, Linkedin, Twitter, Github } from '../../images/SocialMedia';
 
+const SocialMediaWrapper = styled.div`
+  margin-top: ${Dimens.defaultSpacing}
+`;
 
 const Wrapper = styled.div`
-  margin: ${Dimens.defaultSpacing} ${Dimens.defaultSpacing} ${Dimens.boxSeparation};
+  margin: 8px ${Dimens.defaultSpacing} ${Dimens.boxSeparation};
   display: flex;
   flex-direction: row;
   justify-content: center;
 `;
 
+const Link = styled.a`
+  color: #646464;
+  &:hover {
+    filter: contrast(3);
+  }
+`;
+
 class SocialMediaRow extends React.PureComponent {
   render() {
     return (
+      <SocialMediaWrapper>
+      <Link href="mailto:pamepeixinho@gmail.com" target="_top">pamepeixinho@gmail.com</Link>
       <Wrapper>
         <SocialLink to={SocialLinks.facebook} icon={Facebook} />
         <SocialLink to={SocialLinks.instagram} icon={Instagram} />
@@ -24,6 +36,7 @@ class SocialMediaRow extends React.PureComponent {
         <SocialLink to={SocialLinks.twitter} icon={Twitter} />
         <SocialLink to={SocialLinks.github} icon={Github} />
       </Wrapper>
+      </SocialMediaWrapper>
     );
   }
 }
